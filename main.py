696768a463b8066de56b93d25b8746f2883a52f6
Maheshivara <gabrimath@gmail.com>
2023-11-03 20:51:47 -0300

    Criando a base para a tela principal

---
+++ main.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+  running = True
+  while(running):
+    print("Olá, você pode:\n 1- Cadastrar Usuário\n 2- Realizar Login\n 3- Sair")
+
+    try:
+      opt = int(input("Digite a opção desejada: "))
+    except:
+      print("Opa, a opção escolhida não parece válida, por favor tente novamente, caso acredite que isso seja um erro contate o administrador")
+      continue
+
+    match opt:
+      case 1:
+        print("Substituto temporário para tela de cadastro de usuário")
+      case 2:
+        print("Substituto temporário para tela de login de usuário")
+      case 3:
+        print("Obrigado e até a próxima")
+        running = False
+      case _:
+        print("Opção inválida")
