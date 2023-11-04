58dfcef76285eed4d843cdf9b384722eddcec536
Maheshivara <gabrimath@gmail.com>
2023-11-03 21:20:27 -0300

    Criando classe de transação

---
+++ src/classes/transacao.py
@@ -0,0 +1,7 @@
+import datetime
+class Transacao:
+  def __init__(self,tipo:str,data:datetime,valor:float,destino:str) -> None:
+    self.tipo = tipo
+    self.data = data
+    self.valor = valor
+    self.destino = destino
