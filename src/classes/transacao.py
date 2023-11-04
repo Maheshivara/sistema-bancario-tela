61604217df14106e1978390abfce968451dd8e55
Maheshivara <gabrimath@gmail.com>
2023-11-04 16:38:00 -0300

    Criando função para exibir os dados da transação

--- src/classes/transacao.py
+++ src/classes/transacao.py
@@ -5,3 +5,8 @@ class Transacao:
     self.data = data
     self.valor = valor
     self.destino = destino
+  def print(self):
+    """
+    Exibe no terminal de forma legivel as informações da transação
+    """
+    print(f'Data: {self.data.isoformat()}, Tipo: {self.tipo}, Valor: {self.valor:.2f}, Destino: {self.destino}')
