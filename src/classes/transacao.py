9765d98fc81d459d48aa78aee44750748034377e
Maheshivara <gabrimath@gmail.com>
2023-11-04 08:22:17 -0300

    Corrigindo tipo de data

--- src/classes/transacao.py
+++ src/classes/transacao.py
@@ -1,6 +1,6 @@
 import datetime
 class Transacao:
-  def __init__(self,tipo:str,data:datetime.date,valor:float,destino:str) -> None:
+  def __init__(self,tipo:str,data:datetime.datetime,valor:float,destino:str) -> None:
     self.tipo = tipo
     self.data = data
     self.valor = valor
