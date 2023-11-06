9489b61b6dba1e28196f42b3f83d963f165482a0
Maheshivara <gabrimath@gmail.com>
2023-11-06 11:07:03 -0300

    Criando metodo para exibir numero da conta

--- src/classes/numero_conta.py
+++ src/classes/numero_conta.py
@@ -1,4 +1,11 @@
 class Numero_conta:
-  def __init__(self,agencia:int,numero:int) -> None:
+  def __init__(self,agencia:int,numero:float) -> None:
     self.agencia = agencia
     self.numero = numero
+
+  def print(self)->None:
+    """
+    Exibe no terminal a agencia e numero da conta
+    """
+
+    print(f'Agencia: {self.agencia}\nConta: {self.numero}')
