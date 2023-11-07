fcd16c361181a379fe9193a13ae23cd7d244d38f
Maheshivara <gabrimath@gmail.com>
2023-11-07 11:20:53 -0300

    Mudando o tipo para melhor aplicação

--- src/classes/numero_conta.py
+++ src/classes/numero_conta.py
@@ -1,5 +1,5 @@
 class Numero_conta:
-    def __init__(self, agencia: int, numero: float) -> None:
+    def __init__(self, agencia: int, numero: int) -> None:
         self.agencia = agencia
         self.numero = numero
 
